34f2bb3ac6609021101c661fdb4a4cd43f2c856d
salar taeb <salartaeb10@gmail.com>
2020-11-09 13:25:55 +0000

    refactored with promises

---
+++ iss_promised.js
@@ -0,0 +1,19 @@
+// iss_promised.js
+
+// ...
+
+/* 
+ * Input: None
+ * Returns: Promise for fly over data for users location
+ */
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
